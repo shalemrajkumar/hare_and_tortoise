dbaa6d5bf63d3d56f0253dbec46784430a7e9683
shalemrajkumar <shalem20@iitk.ac.in>
2025-01-06 14:09:41 +0530

    added initial files with a type error on arguments

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-```
-nothing here please start coding
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-```
+from src import network, walkers, simulator
+
+def main():
+    pars = {
+    "turtle": {"n": 5, "step": 1, "step_length": 1},
+    "rabbit": {"n": 1, "step": 1, "step_length": 2}
+    }
+
+    # Initialize the simulation
+    sim = simulation(dim=(7, 7), dilution=0.01, total_agents=2, seed=42, truely_random=False, pars=pars)
+    
+    ## simulate 100 iterations of random walk with same parameters
+    sim.simulate(100)
+
+
+if __name__ == "__main__":
+    main()
